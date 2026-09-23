6821dfa25c6befc652c6619c1f6381643093b6ac
swerlajwev <swerlajwev@gmail.com>
2026-09-23 11:55:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "trainingslog-v22";
+const CACHE = "trainingslog-v23";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
